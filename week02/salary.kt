bd06542128b92f7a7934d477393e97cf83a87bf0
ejcart <123350544+ejcart@users.noreply.github.com>
2023-02-09 14:00:55 -0600

    Create salary.kt

---
+++ week02/salary.kt
@@ -0,0 +1,9 @@
+// ej (emmanuel) Carter , 02.4 Name, Age and Annual Income , 2/9/2023  
+fun main() {
+    var firstName = "Joe" 
+    var lastName = "Mahonney"
+    val age = 26
+    val annualPay = 100000.0
+    
+    println("My name is " + firstName + " "+ lastName + ", my age is " + age + " and I hope to earn $" + annualPay + " per year.")
+}
