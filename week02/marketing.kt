bdd9b71088e1e33c17343c87811238ade781a8de
ejcart <123350544+ejcart@users.noreply.github.com>
2023-02-11 14:24:06 -0600

    Create marketing.kt

---
+++ week02/marketing.kt
@@ -0,0 +1,8 @@
+//ej (emmanuel) Carter
+fun main() {
+    var friends  = 400
+    var friendsCount = (friends <= 500)
+    if (friendsCount >= false) { print("Adopt a Dog Today!") } 
+    else { print( "Buy Cat Food At 20% Off") }
+    
+}
