9c7edaa919c1f633cf4a23ebf130e5b32d8ec3af
ejcart <123350544+ejcart@users.noreply.github.com>
2023-02-12 22:55:46 -0600

    Create cookieCalories.kt

---
+++ week02/cookieCalories.kt
@@ -0,0 +1,8 @@
+// ej (emmanuel) Carter Cookie Calorie Calculator 2/12/2023
+import java.util.Scanner
+fun main() {
+    val reader = Scanner(System.`in`)
+    val cals = 300/4
+    val numOfCookies = reader.nextInt()
+    print(cals *  numOfCookies)
+}
