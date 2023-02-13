8a27a56b900621d6b1b054083448a09fe4e6045e
ejcart <123350544+ejcart@users.noreply.github.com>
2023-02-12 23:16:36 -0600

    Create stringMani.kt

---
+++ week02/stringMani.kt
@@ -0,0 +1,10 @@
+//ej (emmanuel) Carter String manipulation 2/12
+import java.util.Scanner
+fun main() {
+    val reader = Scanner(System.`in`)
+    var city = readLine()
+    println(city.uppercase())
+    println(city.lowercase())
+    Println(city.charAt())
+    
+}
