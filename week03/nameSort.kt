aa6e15f6fd3c3466a349775908413ef035932365
ejcart <123350544+ejcart@users.noreply.github.com>
2023-03-31 15:10:04 -0500

    Update nameSort.kt

--- week03/nameSort.kt
+++ week03/nameSort.kt
@@ -1,15 +1,14 @@
 emmanuel ej carter
-fun main() {
-    val reader = Scanner(System.`in`)
+fun main() 
 
     print("Enter the first name: ")
-    val name1 = reader.nextLine()
+    val name1 = readLine()
 
     print("Enter the second name: ")
-    val name2 = reader.nextLine()
+    val name2 =readLine()
 
     print("Enter the third name: ")
-    val name3 = reader.nextLine()
+    val name3 = readLine()
 
     val names = listOf(name1, name2, name3).sorted()
 
