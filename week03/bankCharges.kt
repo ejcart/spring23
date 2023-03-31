ed550ec19a596fb5ac07bd65462f974fc1a0cd4f
ejcart <123350544+ejcart@users.noreply.github.com>
2023-03-31 14:41:19 -0500

    Update bankCharges.kt

--- week03/bankCharges.kt
+++ week03/bankCharges.kt
@@ -1,22 +1,32 @@
 emmanuel carter
-import java.util.Scanner
+class BankCharges {
 
-fun main() {
-    val reader = Scanner(System.`in`)
-    val baseFee = 10
-    var checkFee = 0.0
+    fun main() {
+        val baseFee = 10
+        var checkFee = 0.0
 
-    print("Enter the number of checks written this month: ")
-    val numOfChecks = reader.nextInt()
+        print("Enter the number of checks written this month: ")
+        val checks = readLine()?.toDoubleOrNull()
 
-    when {
-        numOfChecks < 20 -> checkFee = 0.10 * numOfChecks
-        numOfChecks in 20..39 -> checkFee = 0.08 * numOfChecks
-        numOfChecks in 40..59 -> checkFee = 0.06 * numOfChecks
-        numOfChecks >= 60 -> checkFee = 0.04 * numOfChecks
+
+        if (checks != null) {
+            if(checks < 20) {
+                checkFee = 0.10 * checks
+            } else
+                if(checks >20 && checks <= 39) {
+                    checkFee = 0.08 * checks
+                } else
+                    if(checks > 40 && checks <= 59) {
+                        checkFee = 0.06 * checks
+                    } else
+                        if(checks >= 60) {
+                            checkFee = 0.04 * checks
+                        }
+        }
+
+
+        val totalFee = baseFee + checkFee
+
+        println("The bank's service fees for the month are: $$totalFee")
     }
-
-    val totalFee = baseFee + checkFee
-
-    println("The bank's service fees for the month are: $$totalFee")
 }
