a8200d55b1af4ad4f61c37bcce26460150f00c9a
ejcart <123350544+ejcart@users.noreply.github.com>
2023-03-31 15:13:52 -0500

    Update runRace.kt

--- week03/runRace.kt
+++ week03/runRace.kt
@@ -1,20 +1,19 @@
 emmanuel carter
 fun main() {
-    val reader = Scanner(System.`in`)
     print("Enter the name of the first runner: ")
-    val runner1 = reader.nextLine()
+    val runner1 = readLine()
     print("Enter the finishing time for $runner1 (in minutes): ")
-    val time1 = reader.nextInt()
+    val time1 = readLine()?.toIntOrNull()
 
     print("Enter the name of the second runner: ")
-    val runner2 = reader.next()
+    val runner2 = readLine()
     print("Enter the finishing time for $runner2 (in minutes): ")
-    val time2 = reader.nextInt()
+    val time2 = readLine()?.toIntOrNull()
 
     print("Enter the name of the third runner: ")
-    val runner3 = reader.next()
+    val runner3 = readLine()
     print("Enter the finishing time for $runner3 (in minutes): ")
-    val time3 = reader.nextInt()
+    val time3 = readLine()?.toIntOrNull()
 
     val runners = mapOf(runner1 to time1, runner2 to time2, runner3 to time3)
 
