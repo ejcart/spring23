309edddc2497aa29bc2e3aef4aa6a490adb1948a
ejcart <123350544+ejcart@users.noreply.github.com>
2023-03-31 14:56:07 -0500

    Update romans.kt

--- week03/romans.kt
+++ week03/romans.kt
@@ -1,21 +1,39 @@
 emmanuel (ej) carter
-import java.util.Scanner
 fun main() {
-    val reader = Scanner(System.`in`)
-    print("Enter a number between 1 and 10: ")
-    var number = reader.nextInt()
+        print("Enter a number between 1 and 10: ")
+        val number = readLine()?.toIntOrNull()
 
-    when (number) {
-        1 -> println("I")
-        2 -> println("II")
-        3 -> println("III")
-        4 -> println("IV")
-        5 -> println("V")
-        6 -> println("VI")
-        7 -> println("VII")
-        8 -> println("VIII")
-        9 -> println("IX")
-        10 -> println("X")
-        else -> println("Error: Number must be between 1 and 10.")
+        if (number != null) {
+            if (number<=10) {
+                if (number==1 )
+                    println("I")
+                else
+                    if (number==2 )
+                        println("II")
+                if (number==3 )
+                    println("III")
+                else
+                    if (number==4 )
+                        println("IV")
+                    else
+                        if (number==5 )
+                            println("V")
+                        else
+                            if (number==6 )
+                                println("VI")
+                            else
+                                if (number==7 )
+                                    println("VII")
+                                else
+                                    if (number==8 )
+                                        println("VIII")
+                                    else
+                                        if (number==9 ) println("IX")
+                                        else
+                                            if (number==10 ) println("X")
+                                            else
+                                                if(number>10||number<1)
+                                                println("Error: Number must be between 1 and 10.")
+            }
+        }
     }
-}
