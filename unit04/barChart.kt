f3b4e69c7fff2f7bb6a6a53dac3f68755c39afbe
ejcart <123350544+ejcart@users.noreply.github.com>
2023-03-31 17:20:03 -0500

    Create barChart.kt

---
+++ unit04/barChart.kt
@@ -0,0 +1,35 @@
+emmanuel ej carter
+fun main() {
+    println("Enter today's sales for store 1:")
+val store1 = readln().toInt()
+    println("Enter today's sales for store 2:")
+    val store2 = readln().toInt()
+    println("Enter today's sales for store 3:")
+    val store3 = readln().toInt()
+    println("Enter today's sales for store 4:")
+    val store4 = readln().toInt()
+    println("Enter today's sales for store 5:")
+    val store5 = readln().toInt()
+    val x =1
+    for(i in 100..store1 step 100){
+        print("*")
+    }
+    println()
+    for(i in 100..store2 step 100){
+        print("*")
+    }
+    println()
+    for(i in 100..store3 step 100){
+        print("*")
+    }
+    println()
+    for(i in 100..store4 step 100){
+        print("*")
+    }
+    println()
+    for(i in 100..store5 step 100){
+        print("*")
+    }
+    println()
+
+}
