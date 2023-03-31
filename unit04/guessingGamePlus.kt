cbc7cc88f780a0258b8abfc08cc8c8196b236288
ejcart <123350544+ejcart@users.noreply.github.com>
2023-03-31 17:50:01 -0500

    Create guessingGamePlus.kt

---
+++ unit04/guessingGamePlus.kt
@@ -0,0 +1,38 @@
+emmanuel ej carter
+import kotlin.random.Random
+
+fun main() {
+    println("Please pick a number between 1 and 10 ")
+    var choice = readln().toInt()
+    var count=1
+    val cpu = Random.nextInt(1, 10)
+    var guessAgain = true
+    if (choice == cpu) {
+        guessAgain = false
+
+    } else {
+        guessAgain == true
+    }
+
+    while (guessAgain) {
+        if (choice>cpu)
+            println("Too high, try again.")
+        else
+            if (choice<cpu)
+            println("Too low, try again")
+                else
+        if (choice == cpu) {
+            guessAgain = false
+        break
+        }
+        println("Please pick another number between 1 and 10 ")
+         choice = readln().toInt()
+        count++
+
+    }
+    if(!guessAgain){
+        println("congrats You Won!! in $count guesses")
+
+
+    }
+}
