2d9db362c71ebb963fcc9da41b2166daa638695e
ejcart <123350544+ejcart@users.noreply.github.com>
2023-03-31 19:44:57 -0500

    Update salesPrediction.kt

--- week02/salesPrediction.kt
+++ week02/salesPrediction.kt
@@ -1,6 +1,6 @@
 //ej (emmanuel) Carter, SalesPrediction
 fun main() {
     val companySales = 4600000
-val expectedSales = sales * .62   
+val expectedSales = (companySales * 0.62)   
     print(expectedSales )
 }
