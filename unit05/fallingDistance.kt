12d4064e3fe1c7f9270117e625bde83c87979a57
ejcart <123350544+ejcart@users.noreply.github.com>
2023-04-13 09:03:12 -0500

    Update fallingDistance.kt

--- unit05/fallingDistance.kt
+++ unit05/fallingDistance.kt
@@ -3,9 +3,13 @@ import kotlin.math.pow
 
 fun main() {
     print("Please enter time in seconds")
-var time = readln().toDouble()
-    println(fallingDistance(time))
 
+
+for(i in 0..10 ) {
+    var time = i.toDouble()
+    print("$time ")
+    println(fallingDistance(time))
+}
 }
 fun fallingDistance( t: Double): Double {
     val g =9.8
